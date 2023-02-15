b4341e866e3157db55b643b18c8a274864adab5e
Zac Leids <zacleids@gmail.com>
2023-02-14 20:09:10 -0800

    season 16 changes, add new balanced team command

--- src/apex/legend.js
+++ src/apex/legend.js
@@ -1,15 +1,24 @@
 const utils = require('../utils');
 
-// const apexClasses = ['Offensive', 'Defensive', 'Recon', 'Support'];
+// // const apexClasses = ['Offensive', 'Defensive', 'Recon', 'Support'];
+// const apexLegendsCharacters = {
+//   Offensive: ['Bangalore', 'Wraith', 'Mirage', 'Octane', 'Revenant', 'Horizon', 'Fuse', 'Ash', 'Mad Maggie'],
+//   Defensive: ['Gibraltar', 'Caustic', 'Wattson', 'Rampart', 'Newcastle', 'Catalyst'],
+//   Recon: ['Bloodhound', 'Pathfinder', 'Crypto', 'Valkyrie', 'Seer', 'Vantage'],
+//   Support: ['Lifeline', 'Loba']
+// };
+
+// const apexClasses = ['Assault', 'Skirmisher', 'Recon', 'Controller', 'Support'];
 const apexLegendsCharacters = {
-  Offensive: ['Bangalore', 'Wraith', 'Mirage', 'Octane', 'Revenant', 'Horizon', 'Fuse', 'Ash', 'Mad Maggie'],
-  Defensive: ['Gibraltar', 'Caustic', 'Wattson', 'Rampart', 'Newcastle', 'Catalyst'],
-  Recon: ['Bloodhound', 'Pathfinder', 'Crypto', 'Valkyrie', 'Seer', 'Vantage'],
-  Support: ['Lifeline', 'Loba']
+  Assault: ['Ash', 'Fuse', 'Mad Maggie', 'Bangalore', 'Revenant'],
+  Skirmisher: ['Wraith', 'Mirage', 'Octane', 'Horizon', 'Valkyrie', 'Pathfinder'],
+  Recon: ['Bloodhound', 'Crypto', 'Seer', 'Vantage'],
+  Controller: ['Caustic', 'Wattson', 'Rampart', 'Catalyst'],
+  Support: ['Lifeline', 'Loba', 'Gibraltar', 'Newcastle']
 };
 const apexClasses = Object.keys(apexLegendsCharacters);
 
-const apexCharactersList = [].concat(apexLegendsCharacters.Offensive, apexLegendsCharacters.Defensive, apexLegendsCharacters.Recon, apexLegendsCharacters.Support);
+const apexCharactersList = [].concat(apexLegendsCharacters.Assault, apexLegendsCharacters.Skirmisher, apexLegendsCharacters.Recon, apexLegendsCharacters.Controller, apexLegendsCharacters.Support);
 
 const randomLegend = function() {
   const num = utils.randomNumber(0, apexCharactersList.length) - 1;
@@ -39,8 +48,34 @@ const randomTeam = function() {
   return `Random Team: \`${team.join(', ')}\``;
 };
 
+const randomTeamBalanced = function() {
+  const legendsObjectCopy = JSON.parse(JSON.stringify(apexLegendsCharacters));
+  const apexClasses = Object.keys(legendsObjectCopy);
+
+  const classNum1 = utils.randomNumber(0, apexClasses.length) - 1;
+  const class1 = apexClasses.splice(classNum1, 1);
+  const classLegends1 = legendsObjectCopy[class1];
+  const legendNum1 = utils.randomNumber(0, classLegends1.length) - 1;
+  const legend1 = classLegends1[legendNum1];
+
+  const classNum2 = utils.randomNumber(0, apexClasses.length) - 1;
+  const class2 = apexClasses.splice(classNum2, 1);
+  const classLegends2 = legendsObjectCopy[class2];
+  const legendNum2 = utils.randomNumber(0, classLegends2.length) - 1;
+  const legend2 = classLegends2[legendNum2];
+
+  const classNum3 = utils.randomNumber(0, apexClasses.length) - 1;
+  const class3 = apexClasses.splice(classNum3, 1);
+  const classLegends3 = legendsObjectCopy[class3];
+  const legendNum3 = utils.randomNumber(0, classLegends3.length) - 1;
+  const legend3 = classLegends3[legendNum3];
+
+  return `Random Balanced Team: \`\`\`${class1}: ${legend1}\n${class2}: ${legend2}\n${class3}: ${legend3}\n\`\`\``;
+};
+
 module.exports = {
   randomLegend,
   randomClass,
-  randomTeam
+  randomTeam,
+  randomTeamBalanced
 };
